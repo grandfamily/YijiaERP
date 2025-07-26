76d40862cb715f8131e8fa03f662ae9d4c94fb48
grandfamily <grandfamily2521@gmail.com>
2025-07-26 16:10:18 +0800

    Fix syntax error by removing extraneous text

--- src/components/purchase-progress/PurchaseProgress.tsx
+++ src/components/purchase-progress/PurchaseProgress.tsx
@@ -1,6 +1,3 @@
-Looking at this React component file, I can see it's missing several closing brackets. Let me fix the syntax errors by adding the missing closing brackets:
-
-```typescript
 import React, { useState } from 'react';
 import { 
   FileText, 
